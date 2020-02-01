c9ab1c3b16ca21f257561b184304fe527046d56a
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-01-31 21:23:21 -0500

    Scenes & Scene Controller
    [ADD] Added Game & menu Scene
    [ADD] Added SceneController Script
    [ADD] Added Base Singleton class

---
+++ GGJ_2020/Assets/Scripts/Utilities/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T: MonoBehaviour
+{
+    public static T Instance { get; private set; }
+    private static T _instance;
+
+    protected void Awake()
+    {
+        if (_instance != null)
+        {
+            Debug.LogError($"WARNING YOU ARE TRYING TO CREATE NEW INSTANCE OF {typeof(T)} WHEN ONE ALREADY EXISTS");
+            Destroy(gameObject);
+        }
+
+        _instance = this as T;
+    }
+}
