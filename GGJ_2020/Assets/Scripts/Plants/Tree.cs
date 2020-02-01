655a227e10e77ef27cd4fa2a474fad971d06abab
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-02-01 00:02:24 -0500

    Tree Script

---
+++ GGJ_2020/Assets/Scripts/Plants/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : PlantBase
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    //================================================================================================================//
+
+    public override void GrowState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void IdleState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void AttackState()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void DeathState()
+    {
+        throw new System.NotImplementedException();
+    }
+}
