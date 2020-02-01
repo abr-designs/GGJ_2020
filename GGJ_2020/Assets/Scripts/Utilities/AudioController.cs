bcc609f0896dc9ac5ed2179bf3d3ceba2b383c13
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-01-31 22:12:26 -0500

    Prefabs & Audio Controller
    [ADD] Added Singleton Prefabs
    [ADD] Added AudioController Script
    [ADD] Added Recycler system

---
+++ GGJ_2020/Assets/Scripts/Utilities/AudioController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioController : Singleton<AudioController>
+{
+	public enum SOUND
+	{
+		
+	}
+	
+	//================================================================================================================//
+	
+	[SerializeField, ReadOnly]
+	private AudioSource audioSource;
+
+	//================================================================================================================//
+	
+	private void Start()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	//================================================================================================================//
+
+	public void PlaySound(SOUND sound, float volume = 1f)
+	{
+		audioSource.PlayOneShot(GetAudioClip(sound), volume);
+	}
+	
+	public void PlaySound(AudioClip clip, float volume = 1f)
+	{
+		audioSource.PlayOneShot(clip, volume);
+	}
+	
+
+	private AudioClip GetAudioClip(SOUND sound)
+	{
+		switch (sound)
+		{
+			default:
+				throw new NotImplementedException($"{sound} is missing from sounds.");
+		}
+	}
+	
+	//================================================================================================================//
+
+}
