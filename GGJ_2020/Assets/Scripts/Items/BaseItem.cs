a7a9ea03af14e3a2f51c583c4eb655f3174873ed
Randall Romphf <limeonade@hotmail.com>
2020-02-01 00:37:50 -0500

    Added basis for inventory

---
+++ GGJ_2020/Assets/Scripts/Items/BaseItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseItem {
+
+    public enum itemList {
+        bramblePlantSeed,
+        oakWithBeesPlantSeed,
+        slowingFungusPlantSeed,
+        restorativeFlowerPlantSeed
+    }
+
+    public string title;
+    public GameObject prefab;
+    public Dictionary<string, int> stats = new Dictionary<string, int>();
+    
+    public BaseItem(string title) {
+        this.title = title;
+        // this.prefab = prefab;
+    }
+
+    public string getTitle() { return title; }
+    public GameObject getPrefab() { return prefab; }
+
+}
---
+++ GGJ_2020/Assets/Scripts/Items/PlayerInventory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class PlayerInventory : MonoBehaviour
+ {
+     private Dictionary<BaseItem.itemList, int> playerInventory = new Dictionary<BaseItem.itemList, int>();
+
+    void Start() {
+        initInventory();
+        // increment starting stats
+        alterItemQuantity(BaseItem.itemList.oakWithBeesPlantSeed, 5);
+        printInventory();
+    }
+
+     void initInventory() {
+         // initialize inventory by adding empty placeholders for each item type
+        playerInventory.Add(BaseItem.itemList.bramblePlantSeed, 1);
+        playerInventory.Add(BaseItem.itemList.oakWithBeesPlantSeed, 0);
+     }
+
+     public void alterItemQuantity(BaseItem.itemList b, int i) {
+         playerInventory[b] = i;
+     }
+
+    public void printInventory() {
+
+        Debug.Log("Printing Inventory");
+        foreach(KeyValuePair<BaseItem.itemList, int> attachStat in playerInventory)
+        {
+            //Now you can access the key and value both separately from this attachStat as:
+            Debug.Log($"Item: {attachStat.Key} = {attachStat.Value}");
+        }
+
+    }
+
+ }
