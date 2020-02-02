a6d6bf3c08d7c582a7439c6305a79297a01dc0d8
Randall Romphf <limeonade@hotmail.com>
2020-02-01 23:14:52 -0500

    Rejigging enemies for melle attacked

--- GGJ_2020/Assets/Scripts/Enemy Scripts/Pathfinding/Waypoint.cs
+++ GGJ_2020/Assets/Scripts/Enemy Scripts/Pathfinding/Waypoint.cs
@@ -7,5 +7,5 @@ public class Waypoint : MonoBehaviour
     void OnDrawGizmos() {
          Gizmos.color = Color.blue;
          Gizmos.DrawWireCube(transform.position, new Vector3(transform.lossyScale.x, transform.lossyScale.y, transform.lossyScale.z));
- }
+    }
 }
