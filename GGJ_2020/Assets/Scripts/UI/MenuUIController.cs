ff74f7229d50697e7138e3e8a30d96cf38dbb83f
Alex Bedard-Reid <alexbedardreid@gmail.com>
2020-02-02 15:44:35 -0500

    Menu With GGJ Logo & Credits, Fix for Seed Planting

--- GGJ_2020/Assets/Scripts/UI/MenuUIController.cs
+++ GGJ_2020/Assets/Scripts/UI/MenuUIController.cs
@@ -9,45 +9,57 @@ using UnityEngine.UI;
 public class MenuUIController : MonoBehaviour
 {
     //================================================================================================================//
-    
-    [SerializeField, Required]
-    private Button startGameButton;
-    [SerializeField, Required]
-    private Button endGameButton;
-    [SerializeField, Required]
-    private CanvasGroup fadeCanvasGroup;
-    [SerializeField]
-    private float fadeTime = 1f;
-    
+
+    [SerializeField, Required] private Button startGameButton;
+    [SerializeField, Required] private Button creditButton;
+    [SerializeField, Required] private Button backButton;
+    [SerializeField, Required] private Button endGameButton;
+    [SerializeField, Required] private CanvasGroup fadeCanvasGroup;
+    [SerializeField] private float fadeTime = 1f;
+
+    [SerializeField] private GameObject menuWindow;
+    [SerializeField] private GameObject creditsWindow;
+
     //================================================================================================================//
-    
+
     // Start is called before the first frame update
     private void Start()
     {
         fadeCanvasGroup.alpha = 0f;
         startGameButton.onClick.AddListener(StartGame);
         endGameButton.onClick.AddListener(ExitGame);
+
+        creditButton.onClick.AddListener(() =>
+        {
+            menuWindow.SetActive(false);
+            creditsWindow.SetActive(true);
+        });
+        backButton.onClick.AddListener(() =>
+        {
+            menuWindow.SetActive(true);
+            creditsWindow.SetActive(false);
+        });
+
+        menuWindow.SetActive(true);
+        creditsWindow.SetActive(false);
     }
-    
+
     //================================================================================================================//
 
     private void StartGame()
     {
 
-        StartCoroutine(FadeAndTriggerCoroutine(fadeTime, () =>
-        {
-            SceneController.Instance.LoadGameScene(null);
-        }));
+        StartCoroutine(FadeAndTriggerCoroutine(fadeTime, () => { SceneController.Instance.LoadGameScene(null); }));
     }
 
     private void ExitGame()
     {
-        
+
 #if UNITY_EDITOR
         EditorApplication.isPlaying = false;
         return;
 #endif
-        
+
         Application.Quit();
 
     }
@@ -63,10 +75,11 @@ public class MenuUIController : MonoBehaviour
             t += Time.deltaTime;
             yield return null;
         }
-        
+
         fadeCanvasGroup.alpha = 1f;
-        
+
         OnLoadedCallback?.Invoke();
     }
+
     //================================================================================================================//
 }
